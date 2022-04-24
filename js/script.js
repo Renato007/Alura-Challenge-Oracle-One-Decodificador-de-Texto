5edb489180f12cd848059b0bc65d11bbe3b92cf8
Renato Soares <renatosilsoa@gmail.com>
2022-04-24 17:12:58 -0300

    [melhorias] botão criptografar

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-var btn_cripto = document.querySelector("btn-cripto");
+var btn_cripto = document.querySelector("#btn-cripto");
 var campoEntrada = document.querySelector("#input-texto");
+var campoSaida = document.querySelector("#msg");
+var dadoCripitografado
 
 campoEntrada.addEventListener("input", function () {
   texto_entrada = this.value;
@@ -8,6 +10,13 @@ campoEntrada.addEventListener("input", function () {
   var troca02 = criptografar(troca01, "e", "enter")
   var troca03 = criptografar(troca02, "i", "imes")
   var troca04 = criptografar(troca03, "o", "ober")
-  var dadoCripitografado = criptografar(troca04, "u", "ufat")
+  dadoCripitografado = criptografar(troca04, "u", "ufat")
   console.log(dadoCripitografado)
+  
+});
+
+btn_cripto.addEventListener("click", function(event){
+  event.preventDefault();
+  campoSaida.value = dadoCripitografado
+  
 });
