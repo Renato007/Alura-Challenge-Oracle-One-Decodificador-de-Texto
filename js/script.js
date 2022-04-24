201dce5bf214bb6183a4cd7591d1bd594867e7e7
Renato Soares <renatosilsoa@gmail.com>
2022-04-24 16:16:37 -0300

    [funcionalidade] função criptografar

--- js/script.js
+++ js/script.js
@@ -1,20 +1,13 @@
+var btn_cripto = document.querySelector("btn-cripto");
+var campoEntrada = document.querySelector("#input-texto");
 
-/* Regras Codificador: 
-"e" é convertido para "enter" 
-"i" é convertido para "imes"
-"a" é convertido para "ai"
-"o" é convertido para "ober"
-"u" é convertido para "ufat"
-Apenas letras minúsculas
-Não permite acentuação   
-*/
+campoEntrada.addEventListener("input", function () {
+  texto_entrada = this.value;
 
-/* Regras Decodificador: 
-"enter" é convertido para "e" 
-"imes" é convertido para "i"
-"ai" é convertido para "a"
-"ober" é convertido para "o"
-"ufat" é convertido para "u"
-Apenas letras minúsculas
-Não permite acentuação     
-*/
+  var troca01 = criptografar(texto_entrada, "a", "ai")
+  var troca02 = criptografar(troca01, "e", "enter")
+  var troca03 = criptografar(troca02, "i", "imes")
+  var troca04 = criptografar(troca03, "o", "ober")
+  var dadoCripitografado = criptografar(troca04, "u", "ufat")
+  console.log(dadoCripitografado)
+});
